c1b0b97333f41bad83100810f83862060f1eee69
POLETTI Stéphane <polettistephane@msn.com>
2024-06-12 11:45:33 +0200

    fix scss problem refresh (remove extension .scss in imports / add partials / simple form / bootstrap

--- Gemfile
+++ Gemfile
@@ -70,3 +70,8 @@ group :test do
   gem "selenium-webdriver"
 
 end
+
+gem "bootstrap", "~> 5.2"
+gem "simple_form"
+gem "sassc-rails"
+gem 'country_select'
--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,22 +1,21 @@
 class ClientsController < ApplicationController
-  before_action :set_client, only: %i[ show edit update destroy ]
+  before_action :set_client, only: %i[show edit update destroy]
 
   # GET /clients
   def index
     @clients = Client.all
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @clients }
+    end
   end
 
   # GET /clients/1
   def show
-  end
-
-  # GET /clients/new
-  def new
-    @client = Client.new
-  end
-
-  # GET /clients/1/edit
-  def edit
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @client }
+    end
   end
 
   # POST /clients
@@ -24,35 +23,65 @@ class ClientsController < ApplicationController
     @client = Client.new(client_params)
 
     if @client.save
-      redirect_to @client, notice: "Client was successfully created."
+      respond_to do |format|
+        format.html { redirect_to @client, notice: "Client was successfully created." }
+        format.json { render json: @client, status: :created, location: @client }
+      end
     else
-      render :new, status: :unprocessable_entity
+      respond_to do |format|
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @client.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   # PATCH/PUT /clients/1
   def update
     if @client.update(client_params)
-      redirect_to @client, notice: "Client was successfully updated.", status: :see_other
+      respond_to do |format|
+        format.html { redirect_to @client, notice: "Client was successfully updated." }
+        format.json { render json: @client, status: :ok, location: @client }
+      end
     else
-      render :edit, status: :unprocessable_entity
+      respond_to do |format|
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @client.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   # DELETE /clients/1
   def destroy
     @client.destroy
-    redirect_to clients_url, notice: "Client was successfully destroyed.", status: :see_other
+    respond_to do |format|
+      format.html { redirect_to clients_url, notice: "Client was successfully destroyed." }
+      format.json { head :no_content }
+    end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_client
-      @client = Client.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def client_params
-      params.require(:client).permit(:name, :firstname, :email, :phone, :address, :zip_code, :city, :country, :birthdate, :how_do_you_know_us, :notes, :lash_desired_effect, :lash_face_diagnostic, :lash_asymmetry_diagnostic, :lash_eyelid_morphology_diagnostic, :lash_alignment_morphology_diagnostic, :lash_proportion_morphology_diagnostic, :lash_thickness_diagnostic, :lash_length_diagnostic, :lash_curvature_diagnostic, :lash_style, :lash_notes, :lash_mapping, :lash_event, :lash_texture, :lash_density, :lash_extensions_brand, :lash_extensions_curvature, :lash_extensions_thickness, :lash_extensions_glue, :lash_extensions_pretreatment_superbonder, :lash_extensions_pretreatment_booster, :health_diabetes, :health_pregnancy, :health_dry_eyes, :health_teary_eyes, :health_allergy, :health_contact_lenses, :health_surgery, :health_chemotherapy, :health_eyes_allergy, :health_itch, :health_first_application, :health_lie_down, :health_notes)
-    end
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_client
+    @client = Client.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def client_params
+    params.require(:client).permit(
+      :name, :firstname, :email, :phone, :address, :zip_code, :city, :country, 
+      :birthdate, :how_do_you_know_us, :notes, :lash_desired_effect, 
+      :lash_face_diagnostic, :lash_asymmetry_diagnostic, :lash_eyelid_morphology_diagnostic, 
+      :lash_alignment_morphology_diagnostic, :lash_proportion_morphology_diagnostic, 
+      :lash_thickness_diagnostic, :lash_length_diagnostic, :lash_curvature_diagnostic, 
+      :lash_style, :lash_notes, :lash_mapping, :lash_event, :lash_texture, 
+      :lash_density, :lash_extensions_brand, :lash_extensions_curvature, 
+      :lash_extensions_thickness, :lash_extensions_glue, :lash_extensions_pretreatment_superbonder, 
+      :lash_extensions_pretreatment_booster, :health_diabetes, :health_pregnancy, 
+      :health_dry_eyes, :health_teary_eyes, :health_allergy, :health_contact_lenses, 
+      :health_surgery, :health_chemotherapy, :health_eyes_allergy, :health_itch, 
+      :health_first_application, :health_lie_down, :health_notes
+    )
+  end
 end
