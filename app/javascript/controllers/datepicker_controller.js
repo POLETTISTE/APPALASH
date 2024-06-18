5e5bb8beefdcfbc15a4dba16d11f9ad8245bd3e4
POLETTI Stéphane <polettistephane@msn.com>
2024-06-18 09:24:38 +0200

    flatpickr format language and date

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,10 +1,12 @@
-// app/javascript/controllers/datepicker_controller.js
-
 import { Controller } from "@hotwired/stimulus";
-import flatpickr from "flatpickr"; // You need to import this to use new flatpickr()
+import flatpickr from "flatpickr";
+import { French } from "flatpickr/l10n/fr"; // Use correct import path
 
 export default class extends Controller {
   connect() {
-    flatpickr(this.element);
+    flatpickr(this.element, {
+      locale: French, // Set the locale to French
+      dateFormat: "d-m-Y", // Set the desired date format
+    });
   }
 }
--- config/importmap.rb
+++ config/importmap.rb
@@ -10,3 +10,5 @@ pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin 'bootstrap', to: 'https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.esm.js'
 pin '@popperjs/core', to: 'https://unpkg.com/@popperjs/core@2.11.2/dist/esm/index.js'
 pin "flatpickr" # @4.6.13
+pin "flatpickr/l10n/fr", to: "https://ga.jspm.io/npm:flatpickr@4.6.13/dist/l10n/fr.js"
+
