84023de7e263dfa12c8f74a9d1e9faf20f919b29
POLETTI Stéphane <polettistephane@msn.com>
2024-06-18 09:28:50 +0200

    remove comments datepickr

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,12 +1,12 @@
 import { Controller } from "@hotwired/stimulus";
 import flatpickr from "flatpickr";
-import { French } from "flatpickr/l10n/fr"; // Use correct import path
+import { French } from "flatpickr/l10n/fr";
 
 export default class extends Controller {
   connect() {
     flatpickr(this.element, {
-      locale: French, // Set the locale to French
-      dateFormat: "d-m-Y", // Set the desired date format
+      locale: French,
+      dateFormat: "d-m-Y",
     });
   }
 }
