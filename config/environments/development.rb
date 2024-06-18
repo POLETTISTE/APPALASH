77f2a951935e647069f89c49a3621cfc607063b9
POLETTI Stéphane <polettistephane@msn.com>
2024-06-18 13:13:52 +0200

    scss navbar / footer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,6 @@ Rails.application.configure do
 
   config.active_storage.service = :cloudinary
 
-
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
