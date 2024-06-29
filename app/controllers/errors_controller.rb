741951324cf7453b862be3cb8d9e5ce5666fc8a2
POLETTI Stéphane <polettistephane@msn.com>
2024-06-29 16:24:39 +0200

    remove pundit error pages

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,4 +1,7 @@
 class ErrorsController < ApplicationController
+  skip_before_action :authenticate_user!
+  skip_after_action :verify_authorized
+  skip_after_action :verify_policy_scoped
   def not_found
     render status: 404
 
