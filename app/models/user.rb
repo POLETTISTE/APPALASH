c40388b35323b9bd1c54b99e144d73b6bc3d78ac
POLETTI Stéphane <polettistephane@msn.com>
2024-07-04 21:42:15 +0200

    add label, fix missing comas

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :services
-  has_many :clients
-  has_many :transactions
+  has_many :services, dependent: :destroy
+  has_many :clients, dependent: :destroy
+  has_many :transactions, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
