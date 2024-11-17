535b45bd8b7fe2986f523c12c6238a4541801a1e
stephane poletti <polettiweb@gmail.com>
2024-11-17 20:35:46 +0100

    fix css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
+// tailwind.config.js
+
 module.exports = {
   content: [
-    "./app/views/**/*.html.erb",
+    "./app/views/**/*.{html.erb,html.haml,html.slim}", // Ensure views are included
     "./app/helpers/**/*.rb",
-    "./app/assets/stylesheets/**/*.css",
-    "./app/javascript/**/*.js",
+    "./app/assets/stylesheets/application.scss",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-pink": "#FF64A5", // Example custom color
+      },
+    },
   },
   plugins: [],
 };
