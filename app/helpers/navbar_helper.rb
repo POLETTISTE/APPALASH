8fc88497d748a04b8338cc3d564de8a44009919d
stephane poletti <polettiweb@gmail.com>
2024-11-19 15:02:46 +0100

    hamburger menu navbar

--- app/helpers/navbar_helper.rb
+++ app/helpers/navbar_helper.rb
@@ -2,7 +2,12 @@
 
 module NavbarHelper
   def active_class(link_path)
-    current_page?(link_path) ? 'text-custom-pink font-semibold transition duration-200' : 'text-black hover:text-indigo-400 font-semibold transition duration-200'
+    if current_page?(root_path) && link_path == "/dashboard"
+      # If we're at the root and the link is "/dashboard", highlight it as active
+      'text-custom-pink font-semibold transition duration-200'
+    else
+      # For other cases, check if the link matches the current path
+      current_page?(link_path) ? 'text-custom-pink font-semibold transition duration-200' : 'text-black hover:text-indigo-400 font-semibold transition duration-200'
+    end
   end
 end
-
