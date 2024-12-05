b7ba004c8de5134929ec7a88ddfebf71c1b947c5
stephane poletti <polettiweb@gmail.com>
2024-12-05 13:20:48 +0100

    fix alpijne navbar dropdown flash appears

--- app/javascript/controllers/photo_preview_controller.js
+++ app/javascript/controllers/photo_preview_controller.js
@@ -12,8 +12,6 @@ export default class extends Controller {
       // Set the preview image source to the file's data URL
       this.previewTarget.src = event.currentTarget.result;
       this.previewTarget.classList.remove("hidden");
-      // Hide the original avatar image when preview is visible
-      // this.originalAvatarTarget.classList.add("hidden");
     };
 
     // Read the selected file as data URL
@@ -25,7 +23,6 @@ export default class extends Controller {
     // Ensure the preview is hidden and the original avatar is visible if there is no new avatar
     if (this.inputTarget.files.length === 0) {
       this.previewTarget.classList.add("hidden");
-      // this.originalAvatarTarget.classList.remove("hidden");
     }
   }
 }
