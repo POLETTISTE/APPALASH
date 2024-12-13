6280d9ee6959c428ea7c5c8f5785d221d2039fdb
stephane poletti <polettiweb@gmail.com>
2024-12-13 14:38:31 +0100

    fix submit btn form client

--- app/javascript/controllers/edit_client_controller.js
+++ app/javascript/controllers/edit_client_controller.js
@@ -1,12 +1,19 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["info", "form", "editCancelButton"];
+  static targets = [
+    "info",
+    "form",
+    "editCancelButton",
+    "editCancelSubmitButton",
+    "photo",
+  ];
 
   // Define a target for client info sections for easier querying
   get clientInfoTargets() {
     return this.element.querySelectorAll(".client-info");
   }
+
   editCancelToggle() {
     // Loop through each pair of info and form fields
     this.clientInfoTargets.forEach((clientInfoElement) => {
@@ -21,20 +28,28 @@ export default class extends Controller {
       infoElement.classList.toggle("hidden");
       formElement.classList.toggle("hidden");
     });
-    this.button();
+
+    // Toggle visibility for the photo label and input
+    const photoLabel = this.photoTarget.previousElementSibling; // Find the label
+    photoLabel.classList.toggle("hidden");
+    this.photoTarget.classList.toggle("hidden");
+
+    this.editCancelButton();
   }
 
-  button() {
-    // this.editButtonTarget.classList.toggle("hidden");
-    // this.cancelButtonTarget.classList.toggle("hidden");
+  editCancelButton() {
     this.editCancelButtonTarget.classList.toggle("bg-blue-500");
     this.editCancelButtonTarget.classList.toggle("hover:bg-blue-700");
     this.editCancelButtonTarget.classList.toggle("bg-yellow-500");
     this.editCancelButtonTarget.classList.toggle("hover:bg-yellow-700");
-    // Toggle the button's inner text
+
     this.editCancelButtonTarget.innerText =
       this.editCancelButtonTarget.innerText === "MODIFIER"
         ? "ANNULER"
         : "MODIFIER";
+  }
+
+  editCancelSubmitButton() {
+    editCancelSubmitButton.classList.toggle("hidden");
   }
 }
