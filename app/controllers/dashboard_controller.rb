dad09c2044ed757d6f1948ba8d3cccd848f1f452
stephane poletti <polettiweb@gmail.com>
2025-01-17 18:23:40 +0100

    fix application.js + logo sticly

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,29 +2,29 @@
 
 class DashboardController < ApplicationController
   def index
-    @cguests = policy_scope(Guest.all)
+    @guests = policy_scope(Guest.all)
     @services = policy_scope(Service.all)
     @transactions = policy_scope(Transaction.all)
     @users = policy_scope(User.all)
 
-    authorize @cguests
+    authorize @guests
     authorize @services
     authorize @transactions
 
     @non_admin_users = @users.reject(&:admin?) if current_user.admin?
     @admin_users = @users.select(&:admin?) if current_user.admin?
 
-    @cguests_group_by_name = @cguests.group(:name).count
-    @cguests_how_do_you_know_us = @cguests.group(:how_do_you_know_us).count
+    @guests_group_by_name = @guests.group(:name).count
+    @guests_how_do_you_know_us = @guests.group(:how_do_you_know_us).count
 
     # Respond with JSON for guest counts
     respond_to do |format|
       format.html
       format.json do
         response = {
-          guest_counts: @cguests_group_by_name,
-          guest_know_us: @cguests_how_do_you_know_us,
-          guests: @cguests
+          guest_counts: @guests_group_by_name,
+          guest_know_us: @guests_how_do_you_know_us,
+          guests: @guests
         }
         response[:users] = @users if current_user.admin?
 
