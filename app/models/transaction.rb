7cec708081e3d0dd5ade1946b3466b9ad999aec5
stephane poletti <polettiweb@gmail.com>
2025-01-23 17:27:57 +0100

    validations for transaction inside model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,6 +4,15 @@ class Transaction < ApplicationRecord
   belongs_to :guest
   belongs_to :user
   validates :guest_id, presence: true
+  validates :date, presence: true
+  validate :at_least_one_service_selected
+
+    # Custom validation method for services
+    def at_least_one_service_selected
+      if services.blank? || services.all? { |service| service['name'].blank? || service['price'].blank? }
+        errors.add(:services, "must include at least one valid service")
+      end
+    end
 
   # serialize :prestations, Array
 
