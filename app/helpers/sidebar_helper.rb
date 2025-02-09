d6c048bf45ba8d803f7bde08e4b97c3ed42080a1
stephane poletti <polettiweb@gmail.com>
2025-02-09 16:44:04 +0100

    style sidebar and navbar

--- app/helpers/sidebar_helper.rb
+++ app/helpers/sidebar_helper.rb
@@ -5,7 +5,7 @@ module SidebarHelper
     # Check if the current request path is the root path ("/") or the locale-prefixed root ("/en")
     if link_path == '/guests' && (request.path == '/' || request.path == "/#{I18n.locale}")
       # If we are on the root path ("/" or "/en"), apply the active class to "/guests"
-      return 'text-customPink bg-customPink bg-opacity-20 font-semibold border-l-8 border-customPink transition duration-200 text-lg'
+      return 'text-customPink bg-customPink bg-opacity-20 font-semibold border-l-8 border-customPink transition duration-200 text-xl'
     end
 
     # Handle the localized link path, making sure we don't add the locale for the default locale
@@ -13,10 +13,10 @@ module SidebarHelper
 
     # For other links, check if the current path starts with the localized path (accounting for dynamic segments)
     if request.path.start_with?(localized_link_path)
-      return 'text-customPink bg-customPink bg-opacity-20 font-semibold border-l-8 border-customPink transition duration-200 text-lg'
+      return 'text-customPink bg-customPink bg-opacity-20 font-semibold border-l-8 border-customPink transition duration-200 text-xl'
     end
 
     # Default class if not active
-    'text-black hover:text-customPink font-semibold border-l-8 border-transparent transition duration-200 text-lg'
+    'text-black hover:text-customPink font-semibold border-l-8 border-transparent transition duration-200 text-xl'
   end
 end
