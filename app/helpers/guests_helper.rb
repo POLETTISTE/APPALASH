84f1ea279dc456ecb11ada44565b98ea692dc7c7
stephane poletti <polettiweb@gmail.com>
2025-03-10 23:06:59 +0100

    pimp guest page, form and edit fn

--- app/helpers/guests_helper.rb
+++ app/helpers/guests_helper.rb
@@ -1,17 +1,34 @@
-# frozen_string_literal: true
-
-# # frozen_string_literal: true
-
-# app/helpers/guests_helper.rb
 module GuestsHelper
   # GUEST GENERAL
+  def field(form, name, options = {})
+  label = t(".#{name}") # Translates the field label
+  value = form.object.send(name) # Gets the value dynamically
+  id = "guest_#{name}" # Generates the input ID dynamically
+  css_class = options[:class] || "mt-2 block" # Default class for styling
+  toggle_variable = "showForm#{name.to_s.camelize}" # Generates the Alpine.js state variable
+
+  content_tag :div, class: "flex items-center space-x-2" do
+    concat content_tag(:strong, label)
+    concat form.text_field(name, value: value, id: id, class: css_class, 
+      "x-on:keydown.enter" => "#{toggle_variable} = false; $el.closest('form').submit();")
+
+    # Validation Icon (inside the field)
+    concat content_tag(:span, 
+      raw(File.read(Rails.root.join('app', 'assets', 'svg', 'validate.svg'))
+          .gsub('<svg', '<svg class="w-6 h-6 text-customPink"')), 
+      class: "ml-2 cursor-pointer", 
+      "x-on:click" => "#{toggle_variable} = false; $el.closest('form').submit();"
+    )
+  end
+end
+
+
 
   def how_do_you_know_us_options
     I18n.t('guest_options.how_do_you_know_us_options').values
   end
 
-  #  LASH
-
+  # LASH OPTIONS
   def lash_desired_effect_options
     I18n.t('guest_options.lash_desired_effect_options').values
   end
@@ -56,68 +73,3 @@ module GuestsHelper
     I18n.t('guest_options.lash_extensions_glue_options').values
   end
 end
-
-# # app/helpers/s_helper.rb
-# module sHelper
-#   #   **  GENERAL **
-
-#   def how_do_you_know_us_options
-#     ['GOOGLE', 'PLANITY', 'RESEAUX SOCIAUX', 'PROCHE / AMI', 'AUTRE']
-#   end
-
-#   # **  LASH **
-#   # EFFET DESIRE
-#   def lash_desired_effect_options
-#     %w[LEGER INTERMEDIAIRE INTENSE]
-#   end
-
-#   # DIAGNOSTIC VISAGE
-#   def lash_face_diagnostic_options
-#     %w[ROND CARRE OVALE]
-#   end
-
-#   # DIAGNOSTIC ASYMETRIE YEUX
-#   def lash_asymmetry_diagnostic_options
-#     %w[AMANDES RONDS]
-#   end
-
-#   # DIAGNOSTIC PAUPIERES
-#   def lash_eyelid_morphology_diagnostic_options
-#     %w[GRANDES PETITES TOMBANTES]
-#   end
-
-#   # DIAGNOSTIC ALIGNEMENT
-#   def lash_alignment_morphology_diagnostic_options
-#     %w[DESCENDANT ALIGNE MONTANT]
-#   end
-
-#   # DIAGNOSTIC PROPORTION
-#   def lash_proportion_morphology_diagnostic_options
-#     %w[RAPPROCHES EGAUX ECARTES]
-#   end
-
-#   # DIAGNOSTIC DENSITE
-#   def lash_density_options
-#     %w[FORTE MOYENNE FAIBLE]
-#   end
-
-#   #  DIAGNOSTIC STYLE
-#   def lash_style_options
-#     ['NATUREL', 'SQUIRREL', 'DOLLY', 'CAT EYE']
-#   end
-
-#   #  MARQUE DES CILS
-#   def lash_extensions_brand_options
-#     ['LONDON LASH', 'UNE PAUSE POUDREE', 'LEXCILLENCE']
-#   end
-
-#   # COURBURE DES CILS
-#   def lash_extensions_curvature_options
-#     %w[C CC D L]
-#   end
-
-#   # COLLE
-#   def lash_extensions_glue_options
-#     ['NO CONTRAINT', 'LADY BOND', 'LEXCILLENCE']
-#   end
-# end
