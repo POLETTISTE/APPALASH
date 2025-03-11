4d533bfd912531165cf503a642a0488b5bd4660a
stephane poletti <polettiweb@gmail.com>
2025-03-11 08:52:13 +0100

    stash edit work

--- app/helpers/guests_helper.rb
+++ app/helpers/guests_helper.rb
@@ -1,28 +1,17 @@
 module GuestsHelper
   # GUEST GENERAL
   def field(form, name, options = {})
-  label = t(".#{name}") # Translates the field label
-  value = form.object.send(name) # Gets the value dynamically
-  id = "guest_#{name}" # Generates the input ID dynamically
-  css_class = options[:class] || "mt-2 block" # Default class for styling
-  toggle_variable = "showForm#{name.to_s.camelize}" # Generates the Alpine.js state variable
+    label = t(".#{name}") # Translates the field label
+    value = form.object.send(name) # Gets the value dynamically
+    id = "guest_#{name}" # Generates the input ID dynamically
+    css_class = options[:class] || "mt-2 block" # Default class for styling
+    x_show = options[:x_show] || "showForm"  # Add default x_show to control visibility
 
-  content_tag :div, class: "flex items-center space-x-2" do
-    concat content_tag(:strong, label)
-    concat form.text_field(name, value: value, id: id, class: css_class, 
-      "x-on:keydown.enter" => "#{toggle_variable} = false; $el.closest('form').submit();")
-
-    # Validation Icon (inside the field)
-    concat content_tag(:span, 
-      raw(File.read(Rails.root.join('app', 'assets', 'svg', 'validate.svg'))
-          .gsub('<svg', '<svg class="w-6 h-6 text-customPink"')), 
-      class: "ml-2 cursor-pointer", 
-      "x-on:click" => "#{toggle_variable} = false; $el.closest('form').submit();"
-    )
+    content_tag :div, x_show: x_show do
+      concat content_tag(:strong, label)
+      concat form.text_field(name, value: value, id: id, class: css_class)
+    end
   end
-end
-
-
 
   def how_do_you_know_us_options
     I18n.t('guest_options.how_do_you_know_us_options').values
