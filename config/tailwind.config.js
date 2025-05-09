f45cb0a24b78cc804fd8c11eb05919d6538329c8
stephane poletti <polettiweb@gmail.com>
2025-05-09 15:46:22 +0200

    add safe list in tailwind config for helper uses

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
   content: [
     "./app/**/*.{html,erb,js,jsx,ts,tsx,vue}", // Watch all the files in your app
   ],
+  // for example if css in helper, it doesnt this.precompile, so thnaks to it it is precompiled
+  safelist: [
+    "border-b-8", // Safelist border-b-8 (add more dynamic classes here)
+    "border-t-8", // Safelist border-b-8 (add more dynamic classes here)
+    "border-l-8", // Safelist border-b-8 (add more dynamic classes here)
+    "border-r-8",
+    "border-8",
+    "text-lime-500", // Safelist border-b-8 (add more dynamic classes here)
+    // You can add more dynamic classes here if needed, e.g.
+    // 'text-center', 'bg-red-500', 'border-t-4', etc.
+  ],
   theme: {
     extend: {
       colors: {
